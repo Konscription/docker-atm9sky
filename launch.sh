304ad253946ec8aa2a7ca4119312669c0ff17247
Jason McLeod <jason@mcleods.me>
2024-06-19 02:21:22 -0400

    Update to version 1.0.5

--- launch.sh
+++ launch.sh
@@ -12,13 +12,13 @@ else
 	exit 99
 fi
 
-if ! [[ -f 'server-1.0.4.zip' ]]; then
+if ! [[ -f 'server-1.0.5.zip' ]]; then
 	rm -fr config defaultconfigs kubejs mods packmenu *.zip forge*
-	curl -Lo 'server-1.0.4.zip' 'https://edge.forgecdn.net/files/5426/229/server-1.0.4.zip' || exit 9
-	unzip -u -o 'server-1.0.4.zip' -d /data
-	if [[ -d $(echo server-1.0.4.zip | sed 's/.zip//') ]]; then
-		mv -f $(echo server-1.0.4.zip | sed 's/.zip//')/* /data
-		rm -fr $(echo server-1.0.4.zip | sed 's/.zip//')
+	curl -Lo 'server-1.0.5.zip' 'https://edge.forgecdn.net/files/5442/899/server-1.0.5.zip' || exit 9
+	unzip -u -o 'server-1.0.5.zip' -d /data
+	if [[ -d $(echo server-1.0.5.zip | sed 's/.zip//') ]]; then
+		mv -f $(echo server-1.0.5.zip | sed 's/.zip//')/* /data
+		rm -fr $(echo server-1.0.5.zip | sed 's/.zip//')
 	fi
 	curl -Lo forge-${FORGE_VERSION}-installer.jar http://files.minecraftforge.net/maven/net/minecraftforge/forge/$FORGE_VERSION/forge-$FORGE_VERSION-installer.jar
 	java -jar forge-${FORGE_VERSION}-installer.jar --installServer
